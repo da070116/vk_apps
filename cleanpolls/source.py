4362e2282d8b5afa426f79132e8374b46fbaaf41
Alexander Dubrovin <da070116@gmail.com>
2020-07-20 15:24:46 +0300

    working on poll management

--- cleanpolls/source.py
+++ cleanpolls/source.py
@@ -4,8 +4,8 @@ import vk
 class PollsCleaner:
     api = None
 
-    group_digit = '-194461736'
-    group_id = 'club194461736'
+    group_digit = '-197234531'
+    group_id = 'club197234531'
 
     def __init__(self):
         vk_api_token = "f5fb7becf5fb7becf5fb7bece4f589f7fcff5fbf5fb7becab1b8692e9887b40c93c8172"
@@ -13,14 +13,25 @@ class PollsCleaner:
         self.api = vk.API(session)
         top_wall_posts = self.api.wall.get(
             owner_id=self.group_digit,
-            offset=1,
+            offset=0,
             count=5,
             v='5.120'
         )
         for item in top_wall_posts['items']:
             if "attachments" in item.keys() and item["attachments"][0]["type"] == "poll":
                 poll_dict = (item["attachments"][0]["poll"])
-                print(f'{poll_dict=} \n')
+                print(f'owner id: {poll_dict["owner_id"]}')
+                print(f'poll id: {poll_dict["id"]}')
+                answers_id_list = []
+                print(poll_dict['answers'])
+                for answer in poll_dict['answers']:
+                    answers_id_list.append(answer['id'])
+                print(answers_id_list)
+
+                # polls.getVoters
+
+                # for k, v in poll_dict.items():
+                #     print(f"{k}: {v}")
 
 
 poll = PollsCleaner()
